1a762708c68da1ee6f893358b3ec55badc481e9d
githubsneha18 <sneharimipriyanshu@gmail.com>
2023-10-01 20:42:39 +0530

    initial commit

---
+++ src/main/java/org/experiment/build_experiments/App.java
@@ -0,0 +1,13 @@
+package org.experiment.build_experiments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
